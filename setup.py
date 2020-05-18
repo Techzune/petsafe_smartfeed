f8d1b5e4add5886390585b65a0673876f65f9792
Jordan Stremming <jcstremming@gmail.com>
2020-05-18 10:05:10 -0500

    Remove unnecessary username

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="petsafe-smartfeed-techzune",
+    name="petsafe-smartfeed",
     version="1.0.0",
     author="Jordan Stremming",
     author_email="jcstremming@gmail.com",
