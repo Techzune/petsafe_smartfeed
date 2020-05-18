7cd31b1e28d0fec4b85ee1b940638346f8b8ff43
Jordan Stremming <jcstremming@gmail.com>
2020-05-18 14:07:38 -0500

    Actually install properly

--- setup.py
+++ setup.py
@@ -1,22 +1,26 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="petsafe-smartfeed",
-    version="1.1.0",
-    author="Jordan Stremming",
-    author_email="jcstremming@gmail.com",
-    description="Provides ability to connect and control a PetSafe Smart Feed device using the PetSafe-SmartFeed API.",
+    name='petsafe-smartfeed',
+    version='1.1.2',
+    author='Jordan Stremming',
+    license='MIT',
+    author_email='jcstremming@gmail.com',
+    description='Provides ability to connect and control a PetSafe Smart Feed device using the PetSafe-SmartFeed API.',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/techzune/petsafe-smartfeed",
+    long_description_content_type='text/markdown',
+    url='https://github.com/techzune/petsafe-smartfeed',
     packages=setuptools.find_packages(),
+    install_requires=[
+        'requests'
+    ],
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ],
     python_requires='>=3.6',
 )
