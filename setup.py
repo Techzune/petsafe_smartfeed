f3d01e3ca5178883c0bc6169c051b726a1cf2cc9
Jordan Stremming <jcstremming@gmail.com>
2023-03-22 11:51:44 -0500

    update version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="petsafe_smartfeed",
-    version="2.2",
+    version="2.3",
     author="Jordan Stremming",
     license="MIT",
     author_email="jcstremming@gmail.com",
